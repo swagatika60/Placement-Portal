f80475d3fe59daa2557a7a5b9f1f7528bf64b443
Swagatika Beura <swagatikab25@gmail.com>
2026-03-05 20:23:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-123'
-    # This path works on any computer or server
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'placement.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'placement-portal-secret-key-2024'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app', 'placement.db')
